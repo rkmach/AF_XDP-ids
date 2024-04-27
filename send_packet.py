a098649c995b995fcf913b8c512bc20d982ebb49
Raphael Kaviak Machnicki <rkmach17@gmail.com>
2024-04-27 09:51:42 -0300

    Create specific lib for xsk_socket

--- send_packet.py
+++ send_packet.py
@@ -1,9 +1,12 @@
-from scapy.all import TCP, IP, Raw, sendp, Ether, IFACES, UDP
+from scapy.all import TCP, IP, Raw, sendp, Ether, IFACES, UDP, wrpcap
 
 data = 'abddelhadfsdf'
 a = Ether() / IP(src='10.11.1.2', dst='10.11.1.1') / TCP() / Raw(load=data)
-iface = IFACES.dev_from_name("veth0")
+# iface = IFACES.dev_from_name("veth0")
 # a = Ether() / IP(src='192.168.1.68', dst='192.168.1.67') / TCP() / Raw(load=data)
 # iface = IFACES.dev_from_name("enp3s0")
-sendp(a, iface=iface)
-
+# sendp(a, iface=iface)
+wrpcap('teste.pcap', a)
+data = 'taiguara'
+a = Ether() / IP(src='10.11.1.2', dst='10.11.1.1') / TCP() / Raw(load=data)
+wrpcap('teste.pcap', a, append=True)
